a43d4bd300d849869acef8ddac713b5f0442e331
Sal4mander <kamran.gasimov27@gmail.com>
2022-05-02 11:22:01 +0200

    Added initial db

---
+++ NewtonLibraryManager/EntityFramework/DBHandler.cs
@@ -0,0 +1,24 @@
+namespace NewtonLibraryManager.EntityFramework;
+
+public class DBHandler
+{
+    /*
+    public int addCustomer(string inputName, DateTime inputBirthDate, string inputCity, string inputEmail)
+    {
+        using (StoreContext db = new StoreContext())
+        {
+            var Customer = new Customer()
+            {
+
+                Name = inputName,
+                Birthdate = inputBirthDate,
+                City = inputCity,
+                Email = inputEmail
+            };
+            db.Add(Customer);
+            return db.SaveChanges();
+        }
+    }
+}
+*/
+}
