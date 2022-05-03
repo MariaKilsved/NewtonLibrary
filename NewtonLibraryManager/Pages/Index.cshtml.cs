b0bcb298fff0f13003aa770dd2f83d50af058756
MariaKilsved <mkilsved@gmail.com>
2022-05-03 10:20:19 +0200

    How to connect frontend and backend together

--- NewtonLibraryManager/Pages/Index.cshtml.cs
+++ NewtonLibraryManager/Pages/Index.cshtml.cs
@@ -12,7 +12,43 @@ public class IndexModel : PageModel
         _logger = logger;
     }
 
+
+    [BindProperty]
+    public Models.User User { get; set; }
+
+    [BindProperty]
+    private string Password { get; set; }   //Should be hashed before put in User
+
+
     public void OnGet()
     {
+        User = new Models.User { IsAdmin = false };
     }
+
+
+    //Detta kommer köras när man trycker på submit, d.v.s. användaren har skrivit in saker
+    public IActionResult OnPost()
+    {
+        //Om det är något fel på det som skrivits in laddas sidan bara om
+        if(ModelState.IsValid == false)
+        {
+            return Page();
+        }
+
+        //Antagligen ska man lägga in registrering i databasen här.
+        //Kontrollera vad som hämtats från frontend.
+        //Frontend gör antingen register eller login.
+        //Login: User.EMail, Password
+        //Register: User.FirstName, User.LastName, User.EMail, Password
+
+        //Bör hash:a Password innan det läggs i User.Password
+
+        //Bör testa om något av fälten på fronten var IsNullOrWhitespace
+        //Om något var null bör sidan också laddas om d.v.s. Return Page();
+
+        //Gå till annan sida
+        //Kommer använda webbsession senare
+        return RedirectToPage("/ProductSearch");
+    }
+
 }
