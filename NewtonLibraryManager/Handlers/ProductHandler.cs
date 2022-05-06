496008191f62f9a2b27c47badbb73af6966256c5
Sal4mander <kamran.gasimov27@gmail.com>
2022-05-06 13:49:41 +0200

    added global CurrentIdLoggedIn

--- NewtonLibraryManager/Handlers/ProductHandler.cs
+++ NewtonLibraryManager/Handlers/ProductHandler.cs
@@ -1,6 +1,19 @@
+using NewtonLibraryManager.Models;
+using System.Linq;
+
 namespace NewtonLibraryManager.Handlers;
 
 public class ProductHandler
 {
-    
+    public static bool ReturnProduct(string isbn)
+    {
+        using (var db = new NewtonLibraryContext())
+        {
+            var groupedLending = from product in db.Products
+                join lendingDetail in db.LendingDetails on product.Id equals lendingDetail.UserId
+                where product.Isbn.Contains(isbn)
+                select product.Isbn;
+        }
+
+    }
 }
--- NewtonLibraryManager/Models/Product.cs
+++ NewtonLibraryManager/Models/Product.cs
@@ -11,14 +11,14 @@ namespace NewtonLibraryManager.Models
         }
 
         public int Id { get; set; }
-        public string? Title { get; set; }
-        public int? LanguageId { get; set; }
-        public int? CategoryId { get; set; }
-        public int? NrOfCopies { get; set; }
-        public decimal? Dewey { get; set; }
-        public string? Description { get; set; }
-        public string? Isbn { get; set; }
-        public int? ProductType { get; set; }
+        public string Title { get; set; }
+        public int LanguageId { get; set; }
+        public int CategoryId { get; set; }
+        public int NrOfCopies { get; set; }
+        public decimal Dewey { get; set; }
+        public string Description { get; set; }
+        public string Isbn { get; set; }
+        public int ProductType { get; set; }
 
         public virtual LendingDetail LendingDetail { get; set; } = null!;
         public virtual ICollection<AuthorDetail> AuthorDetails { get; set; }
