6cb73f0b5ee799b050698dc36869e2eb3279f9ef
MariaKilsved <mkilsved@gmail.com>
2022-05-09 10:28:31 +0200

    Build error fix

--- NewtonLibraryManager/Handlers/SearchHandler.cs
+++ NewtonLibraryManager/Handlers/SearchHandler.cs
@@ -23,21 +23,31 @@ namespace NewtonLibraryManager.Handlers
                           author.FirstName.Contains(search) ||
                           author.LastName.Contains(search) ||
                           product.Title.Contains(search)
-                    select new
-                    {
-                        TITLE = product.Title,
-                        ISBN = product.Isbn,
-                        FIRSTNAME = author.FirstName,
-                        LASTNAME = author.LastName,
-                    };
+                                select new
+                                {
+                                    TITLE = product.Title,
+                                    LANG = language.Language1,
+                                    CAT = category.Category1,
+                                    DEW = product.Dewey,
+                                    DESC = product.Description,
+                                    ISBN = product.Isbn,
+                                    FIRSTNAME = author.FirstName,
+                                    LASTNAME = author.LastName,
+                                    PRODTYPE = type.Type1
+                                };
 
                 foreach (var item in queryable)
                 {
                     DisplayProductModel displayProductModel = new();
+                    displayProductModel.Category = item.CAT;
+                    displayProductModel.Description = item.DESC;
+                    displayProductModel.Dewey = item.DEW;
+                    displayProductModel.Language = item.LANG;
                     displayProductModel.Title = item.TITLE;
                     displayProductModel.FirstName = item.FIRSTNAME;
                     displayProductModel.LastName = item.LASTNAME;
-                    displayProductModel.ISBN = item.ISBN;
+                    displayProductModel.ProductType = item.PRODTYPE;
+                    displayProductModel.Isbn = item.ISBN;
                     displayProductModels.Add(displayProductModel);
                 }
 
--- NewtonLibraryManager/Models/DisplayProductModel.cs
+++ NewtonLibraryManager/Models/DisplayProductModel.cs
@@ -4,8 +4,13 @@ public class DisplayProductModel
 {
     public int Id { get; set; }
     public string Title { get; set; }
-    public string ISBN { get; set; }
+    public string Language { get; set; }
+    public string Category { get; set; }
+    public decimal Dewey { get; set; }
+    public string Description { get; set; }
+    public string Isbn { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string ProductType { get; set; }
     public string Authors { get { return FirstName + " " + LastName; } }
 }
