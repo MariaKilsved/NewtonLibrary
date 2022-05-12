dcc481e94a896f28ac6a84c0a27b54dd7a1d5de1
Pontus Hedman <pontushedman@MacBook-Pro-som-tillhor-Pontus.local>
2022-05-12 13:24:15 +0200

    Added inventory related functions in inventoryhandler and adminrelatedfunctions in userhandler

---
+++ NewtonLibraryManager/Handlers/InventoryHandler.cs
@@ -0,0 +1,53 @@
+﻿using NewtonLibraryManager.Models;
+using System.Linq;
+using System;
+
+namespace NewtonLibraryManager.Handlers
+{
+	public static class InventoryHandler
+	{
+        //Returnerar antal böcker i lagret
+		public static int getInventoryAmount()
+        {
+            int amt = 0;
+			var products = EntityFramework.Read.ReadHandler.GetProducts();
+            foreach (var item in products)
+            {
+                amt = amt + item.NrOfCopies;
+            }
+            return amt;
+        }
+
+        //Returnerar antal lånade produkter
+        public static int getAmountOfBorrowedProducts()
+        {
+            var products = EntityFramework.Read.ReadHandler.GetLendingDetails();
+            return products.Count();
+        }
+
+        //Returnerar utlånade böcker fårn ett visst ID
+        public static List<LendingDetail> getBorrowedFromProductId(int productId)
+        {
+            var list = EntityFramework.Read.ReadHandler.GetLendingDetails();
+            var lendingDetails = list.Where(x => x.ProductId == productId).ToList();
+            return lendingDetails;
+        }
+
+        //Kollar tidigaste återkommande bok för ett visst ID
+        //och returnerar 
+        public static DateTime? returnsToStock(int productId)
+        {
+            var list = getBorrowedFromProductId(productId);
+            DateTime? borrowedTo = DateTime.MaxValue;
+            foreach (var product in list)
+            {
+                if (product.BorrowedTo < borrowedTo)
+                {
+                    borrowedTo = product.BorrowedTo;
+                }
+            }
+            return borrowedTo;
+        }
+    }
+}
+
