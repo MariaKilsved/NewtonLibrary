da03da6bb8776788f99030d4f29d275ddee545a8
Sal4mander <kamran.gasimov27@gmail.com>
2022-05-20 10:51:01 +0200

    updated GetUserLoans to also display which book the user has loaned

--- NewtonLibraryManager/Handlers/UserHandler.cs
+++ NewtonLibraryManager/Handlers/UserHandler.cs
@@ -40,11 +40,35 @@ namespace NewtonLibraryManager.Handlers
         }
 
 		// also show which product
-		public static List<LendingDetail> GetUserLoans(int userId)
+		public static List<DisplayLoanedProductModel> GetUserLoans(int userId)
 		{
-			List<LendingDetail> lendingDetails = EntityFramework.Read.ReadHandler.GetLendingDetails();
-			lendingDetails = lendingDetails.Where(x => x.UserId == userId).ToList();
-			return lendingDetails;
+			List<DisplayLoanedProductModel> loanedProducts = new();
+
+			using (var db = new NewtonLibraryContext())
+			{
+				var queryable = from ld in db.LendingDetails
+					join usr in db.Users on ld.UserId equals usr.Id
+					join prdct in db.Products on ld.ProductId equals prdct.Id
+					where usr.Id == userId
+					select new
+					{
+						TITLE = prdct.Title,
+						ISBN = prdct.Isbn,
+						FROM = ld.BorrowedFrom,
+						TO = ld.BorrowedTo
+					};
+
+				foreach (var item in queryable)
+				{
+					DisplayLoanedProductModel lpdm = new();
+					lpdm.Title = item.TITLE;
+					lpdm.Isbn = item.ISBN;
+					lpdm.From = item.FROM;
+					lpdm.To = item.TO;
+					loanedProducts.Add(lpdm);
+				}
+			}
+			return loanedProducts;
 		}
 	}
 }
