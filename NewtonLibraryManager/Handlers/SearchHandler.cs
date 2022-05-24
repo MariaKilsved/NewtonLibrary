c5e58509c9d2c5935d036f1d7fe6226bd7c4d7f7
Sal4mander <kamran.gasimov27@gmail.com>
2022-05-24 11:36:39 +0200

    Fixed bug in SearchHandler.cs

--- NewtonLibraryManager/Handlers/SearchHandler.cs
+++ NewtonLibraryManager/Handlers/SearchHandler.cs
@@ -3,9 +3,8 @@ using System.Linq;
 
 namespace NewtonLibraryManager.Handlers
 {
-    public class SearchHandler
+    public static class SearchHandler
     {
-        
         /// <summary>
         /// Method for the search function. Creates a DisplayProduct class from the database based on what the user
         /// searched for. Checks ISBN, Author, Title. Returns a list of these classes, ready to be displayed.
