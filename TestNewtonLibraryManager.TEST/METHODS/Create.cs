df15f8d9bfc520da748464f045598970d80b7d67
4t2g <38842545+4t2g@users.noreply.github.com>
2022-05-30 10:38:57 +0200

    testing environment

---
+++ TestNewtonLibraryManager.TEST/METHODS/Create.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewtonLibraryManager;
+using Xunit;
+
+namespace TestNewtonLibraryManager.TEST.METHODS
+{
+    public class Create
+    {
+        [Fact]
+        public void test()
+        {
+            bool expected = true;
+            bool actual = false;
+
+            if (1 == 1)
+            {
+                actual = true;
+            }
+
+            Assert.Equal(expected, actual);
+        }
+
+        public void CreateAuthor()
+        {
+
+        }
+
+        public void CreateAuthorDetail()
+        {
+
+        }
+
+        public void CreateCategory()
+        {
+
+        }
+
+        public void CreateLanguage()
+        {
+
+        }
+
+        public void CreateLendingDetail()
+        {
+
+        }
+
+        public void CreateReservationDetail()
+        {
+
+        }
+
+        public void CreateProduct()
+        {
+
+        }
+
+        public void CreateType()
+        {
+
+        }
+
+        public void CreateUser()
+        {
+
+        }
+
+    }
+}
---
+++ TestNewtonLibraryManager.TEST/METHODS/Delete.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewtonLibraryManager;
+using Xunit;
+
+namespace TestNewtonLibraryManager.TEST.METHODS
+{
+    public class Delete
+    {
+        [Fact]
+        public void No()
+        {
+            int expected = 5;
+
+            int actual = 4;
+
+            Assert.NotEqual(expected, actual);
+        }
+
+        public void DeleteAuthor()
+        {
+
+        }
+
+        public void DeleteAuthorDetail()
+        {
+
+        }
+
+        public void DeleteCategory()
+        {
+    
+        }
+
+        public void DeleteLanguage()
+        {
+
+        }
+
+        public void DeleteLendingDetail()
+        {
+
+        }
+
+        public void DeleteReservationDetail()
+        {
+
+        }
+
+        public void DeleteProduct()
+        {
+
+        }
+
+        public void DeleteType()
+        {
+
+        }
+
+        public void DeleteUser()
+        {
+
+        }
+
+    }
+}
