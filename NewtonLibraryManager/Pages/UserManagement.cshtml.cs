9a59e52f210eb37101e1d4cf9aef1db0e3b4e49a
MariaKilsved <mkilsved@gmail.com>
2022-06-01 15:40:07 +0200

    Frontend: GetMostActiveBorrower

--- NewtonLibraryManager/Pages/UserManagement.cshtml.cs
+++ NewtonLibraryManager/Pages/UserManagement.cshtml.cs
@@ -8,6 +8,9 @@ namespace NewtonLibraryManager.Pages
         [BindProperty]
         public List<Models.User> Users { get; set; }
 
+        [BindProperty]
+        public string MostActiveUser { get; set; }
+
         /// <summary>
         /// On page loading
         /// </summary>
@@ -15,6 +18,15 @@ namespace NewtonLibraryManager.Pages
         {
             //Get all users, with the admins appearing first
             Users = Handlers.UserHandler.GetUsers().OrderByDescending(x => x.IsAdmin).ToList();
+
+            //Get the most active user, who has the most entries in LendingDetails
+            int activeUserId = Handlers.AggregationHandler.GetMostActiveBorrower();
+            var activeUser = Handlers.UserHandler.GetUser(activeUserId);
+
+            if(activeUser != null)
+            {
+                MostActiveUser = $"{activeUser.FirstName} {activeUser.LastName}";
+            }
         }
     }
 }
