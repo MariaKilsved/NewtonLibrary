116201a03a47e26a7000675cc5c7701b2bcd4953
MariaKilsved <mkilsved@gmail.com>
2022-06-01 15:17:40 +0200

    ProductView: Cancel reservation

--- NewtonLibraryManager/Models/Necategory.cs
+++ NewtonLibraryManager/Models/Necategory.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace NewtonLibraryManager.Models
 {
-    public partial class Necategory
+    public partial class NECategory
     {
         public int Id { get; set; }
         public string Category { get; set; }
