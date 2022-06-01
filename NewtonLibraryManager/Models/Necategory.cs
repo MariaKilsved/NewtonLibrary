e0f5c3b1ea1768787c5cebe2af7d141462cbb5df
FWiman <frewim@gmail.com>
2022-06-01 15:05:33 +0200

    Fixed errors

---
+++ NewtonLibraryManager/Models/Necategory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NewtonLibraryManager.Models
+{
+    public partial class Necategory
+    {
+        public int Id { get; set; }
+        public string Category { get; set; }
+    }
+}
